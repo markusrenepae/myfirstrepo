96ff74e0f82a6a5bd517803698dc9e3a7af83b46
Aksel Allas <allasaksel@gmail.com>
2017-03-02 17:36:01 +0200

    Friend stil here

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Axxel oli siin : ()");
+        System.out.println("Your friend is still here :()");
     }
 }
